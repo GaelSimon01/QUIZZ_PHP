025e3a7449d68f450573802a4ec49014fc744825
Thomas <thomas.rabillon@etu.univ-orleans.fr>
2024-01-20 18:16:10 +0100

    ajout du cookie de session afin de garder le dernier qui a enregistrer un score

--- enregistrer_score.php
+++ enregistrer_score.php
@@ -1,11 +1,16 @@
 <?php
 
+
+session_start();
+
 require_once("./constante.php");
 
 if (isset($_GET['score']) && isset($_GET['prenom']) && isset($_GET['quizz'])) {
     $score = $_GET['score'];
     $nom = $_GET['prenom'];
     $intitule = $_GET['quizz'];
+
+    $_SESSION['nom'] = $nom;
 
     if (count($file_db->query("SELECT * FROM score WHERE nomJoueur='".$nom."' AND intitule='".$intitule."'")->fetchAll()) != 0)
         $file_db->exec("UPDATE score SET point=".$score." WHERE nomJoueur='".$nom."' AND intitule='".$intitule."'");
